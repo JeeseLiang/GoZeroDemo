0c9115628727a4e60f4ee361f5c16107f7a7c403
Jesse <20222005187@m.scnu.edu.cn>
2025-04-18 14:16:45 +0800

    初始化

---
+++ app/article/cmd/api/internal/handler/article/createArticleHandler.go
@@ -0,0 +1,29 @@
+package article
+
+import (
+	"net/http"
+
+	"GoZeroDemo/app/article/cmd/api/internal/logic/article"
+	"GoZeroDemo/app/article/cmd/api/internal/svc"
+	"GoZeroDemo/app/article/cmd/api/internal/types"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+// 创建文章
+func CreateArticleHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.CreateArticleReq
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := article.NewCreateArticleLogic(r.Context(), svcCtx)
+		resp, err := l.CreateArticle(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
